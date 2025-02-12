f3dc3c74b532d367309da42da4d93f1cca788b41
GiovanniMateus <giovannimateus11@gmail.com>
2025-02-12 22:27:16 +0000

    Commit

---
+++ models/cadastro_usuario.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+class CadastroUsuario():
+    def __init__(self, arquivo = 'controlers/database/usuarios.json'):
+        self.arquivo = arquivo
+        self.user = self.load_users()
+        
+    def load_users(self):
+        try:
+            with open(self.arquivo , 'r') as f:
+                return json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return []
+        
+    def add_users(self, nome, senha):
+        new_users = ({'nome': nome, 'senha': senha})
+        self.user.append(new_users)
+        self.save_users()
+        
+    def save_users(self):
+        os.makedirs(os.path.dirname(self.arquivo), exist_ok= True)
+        
+        with open (self.arquivo, 'w') as f:
+            json.dump(self.user, f, indent=4)
