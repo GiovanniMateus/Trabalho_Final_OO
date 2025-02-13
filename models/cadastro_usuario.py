b6e16369a47a60ac43a478c390e425dafe0692e7
GiovanniMateus <giovannimateus11@gmail.com>
2025-02-13 01:01:03 +0000

    sistema de autenticação de usuários

--- models/cadastro_usuario.py
+++ models/cadastro_usuario.py
@@ -1,5 +1,6 @@
 import json
 import os
+import uuid
 
 class CadastroUsuario():
     def __init__(self, arquivo = 'controlers/database/usuarios.json'):
@@ -24,3 +25,27 @@ class CadastroUsuario():
         with open (self.arquivo, 'w') as f:
             json.dump(self.user, f, indent=4)
             
+    def check_users(self, nome, senha):
+        for user in self.user:
+            if user ['nome'] == nome and user ['senha'] == senha:
+                session_id = str(uuid.uuid4())
+                self.authenticated_users[session_id] = user
+                return session_id
+        return None
+    
+    def get_current_user(self, session_id):
+        return self.authenticated_users.get(session_id, None)
+
+    def get_user_name(self, session_id):
+        user = self.get_current_user(session_id)
+        return user['nome'] if user else None
+
+    def get_user_session_id(self, nome):
+        for session_id, user in self.authenticated_users.items():
+            if user['nome'] == nome:
+                return session_id
+        return None
+
+    def logout(self, session_id):
+        if session_id in self.authenticated_users:
+            del self.authenticated_users[session_id]
